09750c55fc43b624cd02988f61cba52976c3e3c4
Alicia Tiernan <aliciatiernan97@gmail.com>
2022-05-30 11:51:55 -0500

    connect mongoose to project

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const express = require('express'); //express
+const express = require("express"); //express
+const path = require("path"); //ejs
+const mongoose = require("mongoose"); //mongo
+const Campground = require('./models/campground');
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); //mongo
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:')); //mongo check for erros
+db.once('open', () => {
+    console.log('Database connected');
+}); //mongo check for errors
+
 const app = express(); //express
-const path = require('path'); //ejs
 
 app.set("views", path.join(__dirname, "views")); //ejs
 app.set("view engine", "ejs"); //ejs
 
-app.get('/', (req, res) =>
-{
-    res.render('home')
-}) //express
+app.get("/", (req, res) => {
+  res.render("home");
+}); //express
 
-app.listen(3000, () =>{
-    console.log('serving on port 3000')
-}) //express
+app.get('/makecampground', async (req, res) =>{
+  const camp = new Campground({title: 'my Backyard'});
+  await camp.save();
+  res.send(camp);
+});
+
+app.listen(3000, () => {
+  console.log("serving on port 3000");
+}); //express
